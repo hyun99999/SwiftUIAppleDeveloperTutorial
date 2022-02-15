8100aa41b222617e860000f15c015f466828bfcf
hyungyu Kim <ypsophila97@icloud.com>
2022-02-15 18:45:26 +0900

    [Feat] set Up Navigation between NavigationList and LandmarkDetail

--- SwiftUIAppleDeveloperTutorial/Landmarks/Views/LandmarkList.swift
+++ SwiftUIAppleDeveloperTutorial/Landmarks/Views/LandmarkList.swift
@@ -9,12 +9,19 @@ import SwiftUI
 
 struct LandmarkList: View {
     var body: some View {
-//        Lists work with identifiable data.
-//        You can make your data identifiable in
-//        one of two ways: by passing along with your data a key path to a property that uniquely identifies each element,
-//        or by making your data type conform to the Identifiable protocol.
-        List(landmarks) { landmark in
-            LandmarkRow(landmark: landmark)
+        NavigationView {
+            //        Lists work with identifiable data.
+            //        You can make your data identifiable in
+            //        one of two ways: by passing along with your data a key path to a property that uniquely identifies each element,
+            //        or by making your data type conform to the Identifiable protocol.
+            List(landmarks) { landmark in
+                NavigationLink {
+                    LandmarkDetail()
+                } label: {
+                    LandmarkRow(landmark: landmark)
+                }
+            }
+            .navigationTitle("Landmarks")
         }
     }
 }
