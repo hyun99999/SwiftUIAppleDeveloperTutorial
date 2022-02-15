11523c2f4724f90f6877694d4b2cde9ac47b67b5
hyungyu Kim <ypsophila97@icloud.com>
2022-02-15 19:36:12 +0900

    [Feat] make filter logic use for showing favorites only

--- SwiftUIAppleDeveloperTutorial/Landmarks/Views/LandmarkList.swift
+++ SwiftUIAppleDeveloperTutorial/Landmarks/Views/LandmarkList.swift
@@ -8,12 +8,20 @@
 import SwiftUI
 
 struct LandmarkList: View {
+    @State private var showFavoritesOnly = false
+    
+    var filteredLandmarks: [Landmark] {
+        landmarks.filter { landmark in
+            (!showFavoritesOnly || landmark.isFavorite)
+        }
+    }
+    
     var body: some View {
         NavigationView {
             //        You can make your data identifiable in
             //        one of two ways: by passing along with your data a key path to a property that uniquely identifies each element,
             //        or by making your data type conform to the Identifiable protocol.
-            List(landmarks) { landmark in
+            List(filteredLandmarks) { landmark in
                 NavigationLink {
                     LandmarkDetail(landmark: landmark)
                 } label: {
@@ -27,10 +35,10 @@ struct LandmarkList: View {
 
 struct LandmarkList_Previews: PreviewProvider {
     static var previews: some View {
-        ForEach(["iPhone SE (2nd generation)", "iPhone XS Max"], id: \.self) { deviceName in
+//        ForEach(["iPhone SE (2nd generation)", "iPhone XS Max"], id: \.self) { deviceName in
             LandmarkList()
-                .previewDevice(PreviewDevice(rawValue: deviceName))
-                .previewDisplayName(deviceName)
-        }
+//                .previewDevice(PreviewDevice(rawValue: deviceName))
+//                .previewDisplayName(deviceName)
+//        }
     }
 }
