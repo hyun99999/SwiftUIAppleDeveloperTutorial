a30bcbffd250e00cfeaf78f21dd259b834762acd
hyungyu Kim <ypsophila97@icloud.com>
2022-02-15 16:33:56 +0900

    [Chore] initialize project

---
+++ SwiftUIAppleDeveloperTutorial/Landmarks/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Landmarks
+//
+//  Created by kimhyungyu on 2022/02/15.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Text("Turtle Rock)
+                .padding()
+        }
+    }
+}
+
+/// declares a preview for that view.
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ SwiftUIAppleDeveloperTutorial/Landmarks/LandmarksApp.swift
@@ -0,0 +1,18 @@
+//
+//  LandmarksApp.swift
+//  Landmarks
+//
+//  Created by kimhyungyu on 2022/02/15.
+//
+
+import SwiftUI
+
+// app's entry point
+@main
+struct LandmarksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
