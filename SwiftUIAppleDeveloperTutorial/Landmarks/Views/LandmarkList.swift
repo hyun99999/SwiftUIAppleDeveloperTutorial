4c16f648d70becfe11ea59dc20b0245281b02a8f
hyungyu Kim <ypsophila97@icloud.com>
2022-02-15 18:53:47 +0900

    [Del] delete remark

--- SwiftUIAppleDeveloperTutorial/Landmarks/Views/LandmarkList.swift
+++ SwiftUIAppleDeveloperTutorial/Landmarks/Views/LandmarkList.swift
@@ -10,7 +10,6 @@ import SwiftUI
 struct LandmarkList: View {
     var body: some View {
         NavigationView {
-            //        Lists work with identifiable data.
             //        You can make your data identifiable in
             //        one of two ways: by passing along with your data a key path to a property that uniquely identifies each element,
             //        or by making your data type conform to the Identifiable protocol.
