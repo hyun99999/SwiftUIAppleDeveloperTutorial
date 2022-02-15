fd430bf96f291d108516f95c781cc548e4daeac0
hyungyu Kim <ypsophila97@icloud.com>
2022-02-15 17:38:05 +0900

    [Feat] landmark model compute location coordinate with CoreLocation framework

--- SwiftUIAppleDeveloperTutorial/Landmarks/Model/Landmark.swift
+++ SwiftUIAppleDeveloperTutorial/Landmarks/Model/Landmark.swift
@@ -7,6 +7,7 @@
 
 import Foundation
 import SwiftUI
+import CoreLocation
 
 struct Landmark: Hashable, Codable {
     var id: Int
@@ -19,5 +20,18 @@ struct Landmark: Hashable, Codable {
     private var imageName: String
     var image: Image {
         Image(imageName)
+    }
+    
+    private var coordinates: Coordinates
+
+    var locationCoordinate: CLLocationCoordinate2D {
+        CLLocationCoordinate2D(
+            latitude: coordinates.latitude,
+            longitude: coordinates.longitude)
+    }
+    
+    struct Coordinates: Hashable, Codable {
+        var latitude: Double
+        var longitude: Double
     }
 }
