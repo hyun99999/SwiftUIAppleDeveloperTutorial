ed7eeb3c084aefab6327842e943540bdd525150d
hyungyu Kim <ypsophila97@icloud.com>
2022-02-15 18:28:03 +0900

    [Feat] dynamic list

--- SwiftUIAppleDeveloperTutorial/Landmarks/Views/LandmarkList.swift
+++ SwiftUIAppleDeveloperTutorial/Landmarks/Views/LandmarkList.swift
@@ -9,9 +9,12 @@ import SwiftUI
 
 struct LandmarkList: View {
     var body: some View {
-        List {
-            LandmarkRow(landmark: landmarks[0])
-            LandmarkRow(landmark: landmarks[1])
+//        Lists work with identifiable data.
+//        You can make your data identifiable in
+//        one of two ways: by passing along with your data a key path to a property that uniquely identifies each element,
+//        or by making your data type conform to the Identifiable protocol.
+        List(landmarks, id: \.id) { landmark in
+            LandmarkRow(landmark: landmark)
         }
     }
 }
