0386c46cabe5cc36b5175428017894b4e39cb7b5
hyungyu Kim <ypsophila97@icloud.com>
2022-02-15 18:31:39 +0900

    [Refactor] make data type conform to the Identifiable protocol.

--- SwiftUIAppleDeveloperTutorial/Landmarks/Views/LandmarkList.swift
+++ SwiftUIAppleDeveloperTutorial/Landmarks/Views/LandmarkList.swift
@@ -13,7 +13,7 @@ struct LandmarkList: View {
 //        You can make your data identifiable in
 //        one of two ways: by passing along with your data a key path to a property that uniquely identifies each element,
 //        or by making your data type conform to the Identifiable protocol.
-        List(landmarks, id: \.id) { landmark in
+        List(landmarks) { landmark in
             LandmarkRow(landmark: landmark)
         }
     }
