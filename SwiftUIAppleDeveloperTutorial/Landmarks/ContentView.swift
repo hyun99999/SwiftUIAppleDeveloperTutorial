2f8315c35b1a00624503d0a6001b3e148bb0a4a4
hyungyu Kim <ypsophila97@icloud.com>
2022-02-15 17:16:13 +0900

    [Feat] combine view

--- SwiftUIAppleDeveloperTutorial/Landmarks/ContentView.swift
+++ SwiftUIAppleDeveloperTutorial/Landmarks/ContentView.swift
@@ -9,18 +9,35 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack(alignment: .leading) {
+        VStack {
+            MapView()
+                .frame(height: 300)
+            
+            CircleImage()
+                .offset(y: -130)
+                .padding(.bottom, -130)
+            VStack(alignment: .leading) {
                 Text("Turtle Rock")
                     .font(.title)
-            HStack {
-                Text("Joshua Tree National Park")
-                    .font(.subheadline)
-                Spacer()
-                Text("California")
-                    .font(.subheadline)
+                HStack {
+                    Text("Joshua Tree National Park")
+                    Spacer()
+                    Text("California")
+                }
+                .font(.subheadline)
+                .foregroundColor(.gray)
+                
+                Divider()
+                
+                Text("About Turtle Rock")
+                    .font(.title2)
+                Text("Descriptive text goes here.")
             }
+            .padding()
+            
+            Spacer()
+            
         }
-        .padding()
     }
 }
 
