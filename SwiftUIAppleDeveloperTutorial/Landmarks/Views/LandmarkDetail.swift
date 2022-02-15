6150ee302c780cad6da0ef36a6de3d15d7ed48e9
hyungyu Kim <ypsophila97@icloud.com>
2022-02-15 19:12:27 +0900

    [Feat] display navigation title

--- SwiftUIAppleDeveloperTutorial/Landmarks/Views/LandmarkDetail.swift
+++ SwiftUIAppleDeveloperTutorial/Landmarks/Views/LandmarkDetail.swift
@@ -37,6 +37,8 @@ struct LandmarkDetail: View {
             }
             .padding()
         }
+        .navigationTitle(landmark.name)
+        .navigationBarTitleDisplayMode(.inline)
     }
 }
 
