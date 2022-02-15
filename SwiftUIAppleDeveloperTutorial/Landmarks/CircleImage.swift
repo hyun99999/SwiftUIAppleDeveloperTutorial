7cadaf00de8add7e08580e862d9e7678daf48c61
hyungyu Kim <ypsophila97@icloud.com>
2022-02-15 17:00:47 +0900

    [Add] add CircleImage through image

---
+++ SwiftUIAppleDeveloperTutorial/Landmarks/CircleImage.swift
@@ -0,0 +1,25 @@
+//
+//  CircleImage.swift
+//  Landmarks
+//
+//  Created by kimhyungyu on 2022/02/15.
+//
+
+import SwiftUI
+
+struct CircleImage: View {
+    var body: some View {
+        Image("turtlerock")
+            .clipShape(Circle())
+            .overlay {
+                Circle().stroke(.gray, lineWidth: 4)
+            }
+            .shadow(radius: 7)
+    }
+}
+
+struct CircleImage_Previews: PreviewProvider {
+    static var previews: some View {
+        CircleImage()
+    }
+}
