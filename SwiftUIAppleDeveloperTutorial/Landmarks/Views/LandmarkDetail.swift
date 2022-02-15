cf56773aa487a690872628a37b62c2319539c090
hyungyu Kim <ypsophila97@icloud.com>
2022-02-15 18:40:10 +0900

    [Refactor] add LandmarkDetail because move code from ContentView

---
+++ SwiftUIAppleDeveloperTutorial/Landmarks/Views/LandmarkDetail.swift
@@ -0,0 +1,48 @@
+//
+//  LandmarkDetail.swift
+//  Landmarks
+//
+//  Created by kimhyungyu on 2022/02/15.
+//
+
+import SwiftUI
+
+struct LandmarkDetail: View {
+    var body: some View {
+        VStack {
+            MapView()
+                .frame(height: 300)
+            
+            CircleImage()
+                .offset(y: -130)
+                .padding(.bottom, -130)
+            VStack(alignment: .leading) {
+                Text("Turtle Rock")
+                    .font(.title)
+                HStack {
+                    Text("Joshua Tree National Park")
+                    Spacer()
+                    Text("California")
+                }
+                .font(.subheadline)
+                .foregroundColor(.gray)
+                
+                Divider()
+                
+                Text("About Turtle Rock")
+                    .font(.title2)
+                Text("Descriptive text goes here.")
+            }
+            .padding()
+            
+            Spacer()
+            
+        }
+    }
+}
+
+struct LandmarkDetail_Previews: PreviewProvider {
+    static var previews: some View {
+        LandmarkDetail()
+    }
+}
