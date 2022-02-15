61e2cc10e5957dfad055656d74d23221305b4e34
hyungyu Kim <ypsophila97@icloud.com>
2022-02-15 18:09:40 +0900

    [Add] add LandmarkList to use LandmarkRow ist

---
+++ SwiftUIAppleDeveloperTutorial/Landmarks/Views/LandmarkList.swift
@@ -0,0 +1,23 @@
+//
+//  LandmarkList.swift
+//  Landmarks
+//
+//  Created by kimhyungyu on 2022/02/15.
+//
+
+import SwiftUI
+
+struct LandmarkList: View {
+    var body: some View {
+        List {
+            LandmarkRow(landmark: landmarks[0])
+            LandmarkRow(landmark: landmarks[1])
+        }
+    }
+}
+
+struct LandmarkList_Previews: PreviewProvider {
+    static var previews: some View {
+        LandmarkList()
+    }
+}
