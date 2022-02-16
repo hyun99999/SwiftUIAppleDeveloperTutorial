450770ef01863bc6f5b509288dc95fe23f214d70
hyungyu Kim <ypsophila97@icloud.com>
2022-02-16 12:15:40 +0900

    [Feat] changes persist when return to the list

--- SwiftUIAppleDeveloperTutorial/Landmarks/Views/Helpers/FavoriteButton.swift
+++ SwiftUIAppleDeveloperTutorial/Landmarks/Views/Helpers/FavoriteButton.swift
@@ -14,6 +14,7 @@ struct FavoriteButton: View {
         Button {
             isSet.toggle()
         } label: {
+            // The title string that you provide for the button’s label doesn’t appear in the UI when you use the iconOnly label style, but VoiceOver uses it to improve accessibility.
             Label("Toggle Favorite", systemImage: isSet ? "star.fill" : "star")
                 .labelStyle(.iconOnly)
                 .foregroundColor(isSet ? .yellow : .gray)
--- SwiftUIAppleDeveloperTutorial/Landmarks/Views/Landmarks/LandmarkDetail.swift
+++ SwiftUIAppleDeveloperTutorial/Landmarks/Views/Landmarks/LandmarkDetail.swift
@@ -8,7 +8,12 @@
 import SwiftUI
 
 struct LandmarkDetail: View {
+    @EnvironmentObject var modelData: ModelData
     var landmark: Landmark
+    
+    var landmarkIndex: Int {
+        modelData.landmarks.firstIndex(where: { $0.id == landmark.id })!
+    }
     
     var body: some View {
         ScrollView {
@@ -19,8 +24,11 @@ struct LandmarkDetail: View {
                 .offset(y: -130)
                 .padding(.bottom, -130)
             VStack(alignment: .leading) {
-                Text(landmark.name)
-                    .font(.title)
+                HStack {
+                    Text(landmark.name)
+                        .font(.title)
+                    FavoriteButton(isSet: $modelData.landmarks[landmarkIndex].isFavorite)
+                }
                 HStack {
                     Text(landmark.park)
                     Spacer()
@@ -43,7 +51,10 @@ struct LandmarkDetail: View {
 }
 
 struct LandmarkDetail_Previews: PreviewProvider {
+    static let modelData = ModelData()
+    
     static var previews: some View {
         LandmarkDetail(landmark: ModelData().landmarks[0])
+            .environmentObject(modelData)
     }
 }
