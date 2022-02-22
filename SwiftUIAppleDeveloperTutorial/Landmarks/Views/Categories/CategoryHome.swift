90afc04044fa1840b40f85ea5b2b39a1908d55f5
hyungyu Kim <ypsophila97@icloud.com>
2022-02-22 15:06:07 +0900

    [Add] add CategoryHome file(display sorted landmarks by category)

---
+++ SwiftUIAppleDeveloperTutorial/Landmarks/Views/Categories/CategoryHome.swift
@@ -0,0 +1,23 @@
+//
+//  CategoryHome.swift
+//  Landmarks
+//
+//  Created by kimhyungyu on 2022/02/22.
+//
+
+import SwiftUI
+
+struct CategoryHome: View {
+    var body: some View {
+        NavigationView {
+            Text("Hello, World!")
+                .navigationTitle("Featured")
+        }
+    }
+}
+
+struct CategoryHome_Previews: PreviewProvider {
+    static var previews: some View {
+        CategoryHome()
+    }
+}
