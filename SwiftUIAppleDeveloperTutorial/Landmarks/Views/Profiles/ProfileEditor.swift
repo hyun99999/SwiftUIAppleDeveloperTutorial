55eb5d2c603e9aa6cf0bb49be48feed3394423fd
hyungyu Kim <ypsophila97@icloud.com>
2022-02-22 19:22:02 +0900

    [Add] add ProfileEditor that act as editable list

---
+++ SwiftUIAppleDeveloperTutorial/Landmarks/Views/Profiles/ProfileEditor.swift
@@ -0,0 +1,28 @@
+//
+//  ProfileEditor.swift
+//  Landmarks
+//
+//  Created by kimhyungyu on 2022/02/22.
+//
+
+import SwiftUI
+
+struct ProfileEditor: View {
+    @Binding var profile: Profile
+    
+    var body: some View {
+        List {
+            HStack {
+                Text("Username").bold()
+                Divider()
+                TextField("Username", text: $profile.username)
+            }
+        }
+    }
+}
+
+struct ProfileEditor_Previews: PreviewProvider {
+    static var previews: some View {
+        ProfileEditor(profile: .constant(.default))
+    }
+}
