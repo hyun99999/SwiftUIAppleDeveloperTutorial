32745680938c33ebbde365196a1a185fc51a7d63
hyungyu Kim <ypsophila97@icloud.com>
2022-02-22 18:05:03 +0900

    [Feat] display user profile when user tap toolbar button

--- SwiftUIAppleDeveloperTutorial/Landmarks/Views/Categories/CategoryHome.swift
+++ SwiftUIAppleDeveloperTutorial/Landmarks/Views/Categories/CategoryHome.swift
@@ -9,6 +9,7 @@ import SwiftUI
 
 struct CategoryHome: View {
     @EnvironmentObject var modelData: ModelData
+    @State private var showingProfile = false
     
     var body: some View {
         NavigationView {
@@ -26,7 +27,19 @@ struct CategoryHome: View {
                     CategoryRow(categoryName: key, items: modelData.categories[key]!)                }
                 .listRowInsets(EdgeInsets())
             }
+            .listStyle(.inset)
             .navigationTitle("Featured")
+            .toolbar {
+                Button {
+                    showingProfile.toggle()
+                } label: {
+                    Label("User Profile", systemImage: "person.crop.circle")
+                }
+            }
+            .sheet(isPresented: $showingProfile) {
+                ProfileHost()
+                    .environmentObject(modelData)
+            }
         }
     }
 }
